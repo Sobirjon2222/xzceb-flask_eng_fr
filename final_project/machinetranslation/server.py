e86fec46d10643e2ce7a18477095e85fa5a1f5c2
Sobirjon2222 <s.khalikov0297@gmail.com>
2023-01-08 14:48:36 -0500

    Final changes

--- final_project/machinetranslation/server.py
+++ final_project/machinetranslation/server.py
@@ -5,10 +5,9 @@ from machinetranslation import translator
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def english_to_french():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    french_text = translator.english_to_french(textToTranslate)
+    french_text= translator.english_to_french(textToTranslate)
     return french_text
 
 @app.route("/frenchToEnglish")
@@ -17,9 +16,8 @@ def french_to_english():
     french_text = translator.french_to_english(textToTranslate)
     return english_text
  
-@app.route("/")
+@app.route("/index")
 def renderIndexPage():
-    # Write the code to render template
     return render_template('index.html')
 
 if __name__ == "__main__":
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,8 +1,9 @@
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('tDe9JIsgrqq-3CfzfTQpwnmZaK-u1licGqdupD3h5tFi')
+authenticator = IAMAuthenticator('9NeWuGjZ4gD7Hc7Rv44ai95nH1j11wuivrIinXcmGxT2')
 language_translator = LanguageTranslatorV3(
     version='2022-12-23',
     authenticator=authenticator
@@ -10,16 +11,13 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url("https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/8a3ea532-e753-4b4a-9d5f-80e9c83e7626")
 
-language_translator.set_disable_ssl_verification(True)
 
 def english_to_french(english_text):
-    '''translation from english to french'''
-    french_text = language_translator.translate(text=english_text,model_id='en-fr').get_result()
-    #print(json.dumps(french_text, indent=2, ensure_ascii=False))
-    return french_text["translations"][0]["translation"]
-
+   translation = language_translator.translate(text=english_text,model_id='en-fr').get_result()
+   french_text=translation ["translations"][0]["translation"]
+   return french_text
+   
 def french_to_english(french_text):
-    '''translation from french to english'''
-    english_text = language_translator.translate(text=french_text,model_id='fr-en').get_result()
-    #print(json.dumps(english_text, indent=2, ensure_ascii=False))
-    return english_text["translations"][0]["translation"]
+    translation = language_translator.translate(text=french_text,model_id='fr-en').get_result()
+    english_text = translation ["translations"][0]["translation"]
+    return english_text 
